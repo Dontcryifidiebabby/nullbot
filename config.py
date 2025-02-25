6fd090494da213e0a68f65e54e6daf09c34b4a19
Dontcryifidiebabby <dontcryifidie@proton.me>
2025-02-25 23:40:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 # config.py
-
 # Токен Telegram-бота от @BotFather
-BOT_TOKEN = "8094518471:AAGbq7pF75_LXMrO8DL7vbOg9F3czNImUPM"  # Замени на СВОЙ токен!!!
+BOT_TOKEN = "8094518471:AAGbq7pF75_LXMrO8DL7vbOg9F3czNImUPM"
 
 # Базовые URL для API (mainnet)
-GASPUMP_API_URL = "https://api.gas111.com/v1"  # API GasPump
+GASPUMP_API_URL = "https://api.gas111.com/v1"  # Проверь реальный URL GasPump
 STONFI_API_URL = "https://api.ston.fi/v1"      # API Ston.fi
 DEDUST_API_URL = "https://api.dedust.io/v2"    # API DeDust
 TON_API_URL = "https://tonapi.io/v2"           # API TON (mainnet)
@@ -13,10 +12,13 @@ TON_API_URL = "https://tonapi.io/v2"           # API TON (mainnet)
 TON_ADDRESS = "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c"
 
 # Кошелёк для комиссии 1% (mainnet)
-COMMISSION_WALLET = "UQCozdQjw3IhYDcWja8lXeqAE91ugnNKl7lQ4o64WsLUi8Qd"  # Замени на свой кошелёк
+COMMISSION_WALLET = "UQCozdQjw3IhYDcWja8lXeqAE91ugnNKl7lQ4o64WsLUi8Qd"
 
 # Процент комиссии (1% = 0.01)
 COMMISSION_RATE = 0.01
 
 # URL манифеста для TON Connect (mainnet)
-TON_CONNECT_MANIFEST_URL = "http://82.146.40.193/tonconnect-manifest.json"  # Актуальный манифест
+TON_CONNECT_MANIFEST_URL = "http://82.146.40.193/tonconnect-manifest.json"
+
+# Ключ API TON (получи на https://tonconsole.com/)
+TON_API_KEY = "AFF74CEAGFAFVOQAAAAOZ2MZNDW6YNU4BZVJMCW6DWKG2HN2YSDUJ2TEV7LNDSAVF7UVACA"  # Замени на реальный ключ
